bf19ae4a5ec5e7e203b2380530810904ff2c7376
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-14 19:14:46 +0200

    edited reconstruct scirpt

--- reconstruct.sh
+++ reconstruct.sh
@@ -9,34 +9,33 @@ PKUMMD_RUNS=(
     runs/actions*/lightning_logs/version_*
 )
 
-for RUN in ${HDM05_RUNS[@]}; do
-    DATA=$(basename $(dirname $(dirname "${RUN}")))
-    DATA="data/hdm05/${DATA}"
+# for RUN in ${HDM05_RUNS[@]}; do
+#     DATA=$(basename $(dirname $(dirname "${RUN}")))
+#     DATA="data/hdm05/${DATA}"
 
-    python reconstruct.py \
-        $RUN \
-        $DATA \
-        --train-split "data/hdm05/2foldsBal_2-class122.txt" \
-        --valid-split "data/hdm05/2foldsBal_1-class122.txt" \
-        --test-split "data/hdm05/2foldsBal_1-class122.txt" \
-        --body-model hdm05 \
-        --fps 12 \
-        --limit 5 \
-        --every-n 100
-done
+#     python reconstruct.py \
+#         $RUN \
+#         $DATA \
+#         --train-split "data/hdm05/2foldsBal_2-class122.txt" \
+#         --valid-split "data/hdm05/2foldsBal_1-class122.txt" \
+#         --test-split "data/hdm05/2foldsBal_1-class122.txt" \
+#         --body-model hdm05 \
+#         --fps 12 \
+#         --limit 5 \
+#         --every-n 100
+# done
 
 for RUN in ${PKUMMD_RUNS[@]}; do
-    DATA=$(basename $(dirname $(dirname "${RUN}")))
-    DATA="data/pku-mmd/${DATA}"
+    DATA="/home/drking/Documents/bakalarka/data/pku-mmd/actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10.data"
 
     python reconstruct.py \
         $RUN \
         $DATA \
-        --train-split "data/pku-mmd/CS_train_objects_messif-lines.txt" \
-        --valid-split "data/pku-mmd/CS_test_objects_messif-lines.txt" \
-        --test-split "data/pku-mmd/CS_test_objects_messif-lines.txt" \
+        --train-split "/home/drking/Documents/bakalarka/data/pku-mmd/splits/CS_train_objects_messif-lines.txt" \
+        --valid-split "/home/drking/Documents/bakalarka/data/pku-mmd/splits/CS_test_objects_messif-lines.txt" \
+        --test-split "/home/drking/Documents/bakalarka/data/pku-mmd/splits/CS_test_objects_messif-lines.txt" \
         --body-model pku-mmd \
-        --fps 30 \
+        --fps 10 \
         --limit 5 \
         --every-n 100
 done
