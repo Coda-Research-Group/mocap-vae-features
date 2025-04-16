5ea8ce2ff98cb85d053d9a18e7681f3388e93634
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-16 17:14:45 +0200

    Script for converting into MWs test config

---
+++ Implementation-Prochazka/code/clustering/scripts/convert-scl-to-mw-final.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#PBS -l walltime=24:0:0
+#PBS -l select=1:ncpus=4:mem=32gb:scratch_local=50gb
+#PBS -o /dev/null
+#PBS -e /dev/null
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+cd /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/motionvocabulary/dist/lib || exit
+
+
+CLS_OBJ="messif.objects.impl.ObjectFloatVectorCosine"
+SOFTASSIGNPARAM="D0K1"   # optional (default is D0K1)
+TOSEQ="--tosequence"   # set if you need to convert the input file of segments to motion words _and_ merge the segments back to sequences/actions
+MEMORY="16g"
+JDK_PATH="/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java"
+VOCTYPE='-v'
+
+
+CLASSPATH=${CLASSPATH:-'MESSIF.jar:MESSIF-Utility.jar:MotionVocabulary.jar:commons-cli-1.4.jar:smf-core-1.0.jar:smf-impl-1.0.jar:MCDR.jar:m-index.jar:trove4j-3.0.3.jar'}
+
+#java -Xmx${MEM:-500m} -cp $CLASSPATH messif.motionvocabulary.MotionVocabulary -d $DATAFILE -c $CLS_OBJ --quantize $TOSEQ ${VOCTYPE} $VOCABULARY $SOFTASSIGNPARAM --output $OUTPUT
+
+
+function convert() {
+    echo 'convertBodyPart'
+
+	CLUSTER_FOLDER_NAME=$(basename "${CLUSTER_FOLDER_PATH}")
+
+    mkdir -p "${OUTPUT_ROOT_PATH}"
+
+      COMMAND="\
+  ${JDK_PATH} \
+  -Xmx${MEMORY} \
+  -cp ${CLASSPATH} \
+  messif.motionvocabulary.MotionVocabulary \
+  -d ${DATAFILE} \
+  -c ${CLS_OBJ} \
+  --quantize ${TOSEQ} ${VOCTYPE} ${CLUSTER_FOLDER_PATH}/medoids.txt \
+  --soft-assign ${SOFTASSIGNPARAM} \
+  --output ${OUTPUT_ROOT_PATH}/${PART}.${SOFTASSIGNPARAM} \
+  "
+
+      echo "${COMMAND}"
+
+      eval "${COMMAND}"
+}
+#for DIM in "64" "32" "16" "8" "4"; do
+#  	for BETA in "0.1" "1" "10"; do
+# 		for K in "5" "10" "20" "50" "100" "150" "200" "250" "300" "350" "400" "500" "600" "750" "1000"; do
+#			for PART in "hdm05-legR" "hdm05-legL" "hdm05-handR" "hdm05-handL" "hdm05-torso"; do
+#				echo "${PART}"
+#
+#		  	    DATAFILE="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/predictions_segmented_model=${PART}.data"
+#				OUTPUT_ROOT_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}"
+#				CLUSTER_FOLDER_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/clusters-${PART}/KMedoidsFastPAM--kmeans.k_${K}"
+#				convert
+#
+#			done
+#		done
+#	done
+#done
+#
+#for DIM in "256" "128" "64" "32" "16" "8" "4" "2"; do
+#  	for BETA in "0.1" "1" "10"; do
+# 		for K in "5" "10" "20" "50" "100" "150" "200" "250" "300" "350" "400" "500" "600" "750" "1000"; do
+#			for PART in "hdm05"; do
+#				echo "${PART}"
+#
+#		  	    DATAFILE="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/predictions_segmented_model=${PART}.data"
+#				OUTPUT_ROOT_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-full/KMeansPivotChooser--kmeans.k_${K}"
+#				CLUSTER_FOLDER_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/clusters-${PART}/KMedoidsFastPAM--kmeans.k_${K}"
+#				convert
+#
+#			done
+#		done
+#	done
+#done
+
+# Test model
+for DIM in "64"; do
+  	for BETA in "1"; do
+ 		for K in "10"; do
+			for PART in "hdm05-legR" "hdm05-legL" "hdm05-handR" "hdm05-handL" "hdm05-torso"; do
+				echo "${PART}"
+
+		  	    DATAFILE="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/predictions_segmented_model=${PART}.data"
+				OUTPUT_ROOT_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}"
+				CLUSTER_FOLDER_PATH="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/clusters-${PART}/KMedoidsFastPAM--kmeans.k_${K}"
+				convert
+
+			done
+		done
+	done
+done
+echo "Job finished successfully!"
