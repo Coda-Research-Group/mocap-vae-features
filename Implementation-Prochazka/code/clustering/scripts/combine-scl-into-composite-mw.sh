971afd580a430662b07b172caeaf3ad96b05379e
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-19 01:28:26 +0200

    added remote combiner to CMWs

--- Implementation-Prochazka/code/clustering/scripts/combine-scl-into-composite-mw.sh
+++ Implementation-Prochazka/code/clustering/scripts/combine-scl-into-composite-mw.sh
@@ -10,7 +10,7 @@ COMBINER_JAR_PATH=''
 
 ## Defaults
 
-JDK_PATH=${JDK_PATH:-'/usr/bin/java'}
+JDK_PATH=${JDK_PATH:-'/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java'}
 COMBINER_JAR_PATH=${COMBINER_JAR_PATH:-'/home/drking/Documents/bakalarka/mocap-vae-features/Implementation-Prochazka/code/clustering/jars/compositeMWCombiner.jar'}
 
 ## Functions
@@ -29,18 +29,22 @@ ${JDK_PATH} \
     echo "${COMMAND}"
 
     # 5 BPs
-    eval "${COMMAND}" >"${BODY_PART_MWS_FOLDER}/../${FILE_NAME}.composite"
+    eval "${COMMAND}" >"/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/composites/Elki_${DIM}_${BETA}_${K}.composite"
     # 5 BPs + 3 relations
     # eval "${COMMAND}" >"${BODY_PART_MWS_FOLDER}/../${FILE_NAME}.composite-extended"
 }
 
 # HDM05-130
-for K in '350'; do
+for DIM in "64" "32" "16" "8" "4"; do
+  	for BETA in "0.1" "1" "10"; do
+ 		for K in "5" "10" "20" "50" "100" "150" "200" "250" "300" "350" "400" "500" "600" "750"; do
 
-	BODY_PART_MWS_FOLDER="/home/drking/Documents/bakalarka/data/SCL/composite/clusters/fold-mws/KMeansPivotChooser--kmeans.k_${K}"
+            BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}"
 
-    combineBodyPartsIntoCompositeMW
+            combineBodyPartsIntoCompositeMW
 
+        done
+    done
 done
 
 ## HDM05-65
