52e9120f18fea84e4d985bbc16a17a654010a1ca
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-22 18:55:38 +0200

    added run script for running 0 clusters

---
+++ idk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/warning-search.py > /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/cluster-issues.txt
