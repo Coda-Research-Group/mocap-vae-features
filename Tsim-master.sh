548e539d4036831e6ffdaad538f629ebd82a22c1
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-27 21:47:12 +0200

    addded Tsim calc for hdm05 all

---
+++ Tsim-master.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#PBS -l walltime=48:0:0
+#PBS -l select=1:ncpus=4:mem=64gb
+
+
+# TODO: replace LOGIN with your login
+# TODO: select a particular cluster: https://metavo.metacentrum.cz/pbsmon2/nodes/pbs
+
+
+REPO_DIR='/storage/brno12-cerit/home/drking/experiments'
+
+
+cd "${REPO_DIR}" || {
+    echo >&2 "Repository directory ${REPO_DIR} does not exist!"
+    exit 1
+}
+
+
+
+for EXP in "all" ; do
+    for DIM in "256" "128" "64" "32" "16" "8" "4" "2" "1"; do
+        for BETA in "0.1" "1" "10"; do
+
+            python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Tsim.py /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/predictions_segmented_model=hdm05.data > /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/Tsims.txt
+
+        done
+    done
+done
+
+wait
