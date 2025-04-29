bba0e25881bb8d055016f7de4a9897e0c29ec318
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-30 00:10:40 +0200

    added tester script

---
+++ tester-master.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -l walltime=24:0:0
+#PBS -l select=1:ncpus=4:mem=32gb:scratch_local=50gb
+#PBS -o /dev/null
+#PBS -e /dev/null
+
+
+JDK_PATH='/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java'
+
+
+for EXP in "cv"; do
+    for DIM in "256"; do
+        for BETA in "1"; do
+            for MODEL in "pku-mmd"; do
+                for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000"; do
+
+                    COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
+-fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}/${MODEL}.D0K1 \
+-k 4 \
+-cv \
+-dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
+"
+                    echo "${COMMAND}"
+
+                    mkdir -p "/storage/brno12-cerit/home/drking/experiments/results/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}"
+
+                    echo "=== Running with K=${K} ===" >> "/storage/brno12-cerit/home/drking/experiments/results/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/results.txt"
+
+                    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/results.txt"
+
+                done
+            done
+        done
+    done
+done
