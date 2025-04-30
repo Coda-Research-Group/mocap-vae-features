0159d738773b591e0c4e3f7a68b12334f3cba264
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-30 17:58:17 +0200

    new setup for evaluation

--- tester-master.sh
+++ tester-master.sh
@@ -7,14 +7,6 @@
 
 JDK_PATH='/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java'
 
-# avail_classes 
-
-# SequenceMotionWordsDTW.class;
-# SequenceMotionWordsNMatchesDTW.class;
-# SequenceMotionWordsSoftAssignmentDTW.class;
-# SequenceMotionWordsNGramsJaccard.class;
-# SequenceSegmentCodeListDTW.class;
-
 WORKER_SCRIPT_PATH="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single.sh"
 WORKER_SCRIPT_PATH_NORM="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-norm.sh"
 
@@ -22,14 +14,14 @@ for EXP in "cv" "cs"; do
     for DIM in "64" "32" "16" "8" "4"; do
         for BETA in "0.1" "1" "10"; do
 
-            JOB_NAME="vae_${EXP}_${DIM}_${BETA}"
+            JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}"
             echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
             qsub \
                 -N "${JOB_NAME}" \
                 -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
                 "${WORKER_SCRIPT_PATH}"
 
-            JOB_NAME="vae_${EXP}_${DIM}_${BETA}_norm"
+            JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_norm"
             echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
             qsub \
                 -N "${JOB_NAME}" \
--- tester-single.sh
+++ tester-single.sh
@@ -12,14 +12,6 @@ BETA=${PASSED_BETA}
 EXP=${PASSED_EXP}
 
 
-# avail_classes 
-
-# SequenceMotionWordsDTW.class;
-# SequenceMotionWordsNMatchesDTW.class;
-# SequenceMotionWordsSoftAssignmentDTW.class;
-# SequenceMotionWordsNGramsJaccard.class;
-# SequenceSegmentCodeListDTW.class;
-
 
 for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000"; do
 
@@ -27,7 +19,7 @@ for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000";
 -fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}.composite \
 -k 4 \
 -cv \
--cls SequenceMotionWordsNMatchesDTW; \
+--nmatches 2 \
 -dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
 "
     echo "${COMMAND}"
@@ -42,7 +34,7 @@ for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000";
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
 -fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}.composite \
 -cv \
--cls SequenceMotionWordsNMatchesDTW; \
+--nmatches 2 \
 -dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
 "
     echo "${COMMAND}"
