9b736b5cb40dee39c7a536b5b289be7e5478b7c9
Tomáš Drkoš <536447@mail.muni.cz>
2025-04-30 19:15:23 +0200

    fixed paths

--- tester-single-full.sh
+++ tester-single-full.sh
@@ -16,7 +16,7 @@ EXP=${PASSED_EXP}
 for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000"; do
 
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
--fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}/pku-mmd.D0K1 \
+-fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-full/KMeansPivotChooser--kmeans.k_${K}/pku-mmd.D0K1 \
 -k 4 \
 -cv \
 --nmatches 2 \
@@ -32,7 +32,7 @@ done
 for K in "1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000"; do
 
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
--fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-parts/KMeansPivotChooser--kmeans.k_${K}/pku-mmd.D0K1 \
+-fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/pku-mmd/${EXP}/lat_dim=${DIM}_beta=${BETA}/MWs-full/KMeansPivotChooser--kmeans.k_${K}/pku-mmd.D0K1 \
 -cv \
 --nmatches 2 \
 -dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
