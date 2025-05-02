8162125bb90fa0ffee1b896e8d86141131d66c11
Tomáš Drkoš <536447@mail.muni.cz>
2025-05-02 13:13:21 +0200

    test hdm MO

--- tester-master.sh
+++ tester-master.sh
@@ -12,27 +12,27 @@ WORKER_SCRIPT_PATH_NORM="/storage/brno12-cerit/home/drking/experiments/mocap-vae
 WORKER_SCRIPT_PATH_FULL="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-full.sh"
 WORKER_SCRIPT_PATH_SCL="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-scl.sh"
 
-for EXP in "cs"; do
-    for DIM in "64" "32" "16" "8" "4"; do
-        for BETA in "0.1" "1" "10"; do
+# for EXP in "cs"; do
+#     for DIM in "64" "32" "16" "8" "4"; do
+#         for BETA in "0.1" "1" "10"; do
 
-            JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}"
-            echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
-            qsub \
-                -N "${JOB_NAME}" \
-                -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                "${WORKER_SCRIPT_PATH}"
+#             JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}"
+#             echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
+#             qsub \
+#                 -N "${JOB_NAME}" \
+#                 -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
+#                 "${WORKER_SCRIPT_PATH}"
 
-            JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_norm"
-            echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
-            qsub \
-                -N "${JOB_NAME}" \
-                -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                "${WORKER_SCRIPT_PATH_NORM}"
+#             JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_norm"
+#             echo "Submitting job for EXP=${EXP}, DIM=${DIM}, BETA=${BETA}"
+#             qsub \
+#                 -N "${JOB_NAME}" \
+#                 -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
+#                 "${WORKER_SCRIPT_PATH_NORM}"
 
-        done
-    done
-done
+#         done
+#     done
+# done
 
 for EXP in "cs"; do
     for DIM in "256" "128" "64" "32" "16" "8" ; do
@@ -43,7 +43,7 @@ for EXP in "cs"; do
             qsub \
                 -N "${JOB_NAME}" \
                 -v "PASSED_EXP=${EXP},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                "${WORKER_SCRIPT_PATH_FULL}"
+                "${WORKER_SCRIPT_PATH}"
 
         done
     done
--- tester-single.sh
+++ tester-single.sh
@@ -13,30 +13,30 @@ EXP=${PASSED_EXP}
 
 
 
-for K in "50" "100" "150" "200" "250" "300" "350" "400" "500" "600" "750" "1000" "1500" "2000"; do
+for K in "350" "1000" ; do
 
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
--fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-parts-M/KMeansPivotChooser--kmeans.k_${K}.composite \
+-fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-full-MO/KMeansPivotChooser--kmeans.k_${K}.composite \
 -k 4 \
 --nmatches 2 \
 -dd /storage/brno12-cerit/home/drking/data/hdm05/category_description.txt \
 "
     echo "${COMMAND}"
     mkdir -p "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}"
-    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}/results.txt"
+    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}/results-MO.txt"
 
 done
 
 
-for K in "50" "100" "150" "200" "250" "300" "350" "400" "500" "600" "750" "1000" "1500" "2000"; do
+for K in "350" "1000"; do
 
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
--fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-parts-M/KMeansPivotChooser--kmeans.k_${K}.composite \
+-fp /storage/brno12-cerit/home/drking/experiments/SCL-segmented-actions/hdm05/all/lat_dim=${DIM}_beta=${BETA}/MWs-full-MO/KMeansPivotChooser--kmeans.k_${K}.composite \
 --nmatches 2 \
 -dd /storage/brno12-cerit/home/drking/data/hdm05/category_description.txt \
 "
     echo "${COMMAND}"
     mkdir -p "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}"
-    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}/results.txt"
+    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/lat_dim=${DIM}_beta=${BETA}/results-MO.txt"
 
 done
