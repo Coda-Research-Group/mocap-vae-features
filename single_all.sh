47d68cfdec1bd6744dce235f97ee6edd1687a3fc
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-10-01 11:43:49 +0200

    removed cuda from master

--- single_all.sh
+++ single_all.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=$PBS_NUM_PPN # set it equal to PBS variable PBS_NUM_PPN (number of CPUs in a chunk)
-export GPUS=$CUDA_VISIBLE_DEVICES
-
-echo "image: ${SINGULARITY_IMG}"
-echo "CPUs: ${OMP_NUM_THREADS}"
-echo "GPUs: ${GPUS}"
-
 SCRIPT_DIR='/storage/brno12-cerit/home/drking/experiments/mocap-vae-features'
 PBS_LOGS_DIR='/storage/brno12-cerit/home/drking/experiments/pbs'
 REPO_DIR='/storage/brno12-cerit/home/drking/experiments'
 ENV_NAME='cuda4'
 
-DIMS=("256" "128" "64" "32" "16" "8" "4")
-BETAS=("0.1" "1" "10")
-ITERS=("1" "2" "3")
-
-# PATHS to scripts
 WORKER_SCRIPT_PATH="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/single_config.sh"
 
-
-module add conda-modules
-module add mambaforge
 
 cd "${REPO_DIR}" || {
     echo >&2 "Repository directory ${REPO_DIR} does not exist!"
     exit 1
-}
-
-conda activate "/storage/brno12-cerit/home/drking/.conda/envs/${ENV_NAME}" || {
-    echo >&2 "Conda environment does not exist!"
-    exit 2
 }
 
 
