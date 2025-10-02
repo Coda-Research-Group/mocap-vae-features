43adcbadee864676583e508e51f94a50eed1fa57
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-10-02 15:44:25 +0200

    remaked config script for hdm05

--- single_config.sh
+++ single_config.sh
@@ -6,16 +6,6 @@
 #PBS -o /dev/null
 #PBS -e /dev/null
 
-# TODO: replace LOGIN with your login
-# TODO: select a particular cluster: https://metavo.metacentrum.cz/pbsmon2/nodes/pbs
-
-export OMP_NUM_THREADS=$PBS_NUM_PPN # set it equal to PBS variable PBS_NUM_PPN (number of CPUs in a chunk)
-export GPUS=$CUDA_VISIBLE_DEVICES
-
-echo "image: ${SINGULARITY_IMG}"
-echo "CPUs: ${OMP_NUM_THREADS}"
-echo "GPUs: ${GPUS}"
-
 SCRIPT_DIR='/storage/brno12-cerit/home/drking/experiments/mocap-vae-features'
 PBS_LOGS_DIR='/storage/brno12-cerit/home/drking/experiments/pbs'
 REPO_DIR='/storage/brno12-cerit/home/drking/experiments'
@@ -47,17 +37,17 @@ if [ "${MOD}" -eq "hdm05" ]; then
 
     for DIM in "256" "128" "64" "32" "16" "8" "4"; do
         for BETA in "0.1" "1" "10"; do
-            python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/train.py --multirun exp=hdm05/${MOD} \
-                latent_dim=${DIM} beta=${BETA} iteration=${ITER} body_model=hdm05 > /dev/null 2>&1
+            python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/train.py --multirun exp=hdm05/all \
+                latent_dim=${DIM} beta=${BETA} iteration=${ITER} body_model=${MOD} > /dev/null 2>&1
         done
     done
 
-else 
+else
 
     for DIM in "64" "32" "16" "8" "4"; do
         for BETA in "0.1" "1" "10"; do
-            python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/train.py --multirun exp=hdm05/${MOD} \
-                latent_dim=${DIM} beta=${BETA} iteration=${ITER} body_model=hdm05 > /dev/null 2>&1
+            python /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/train.py --multirun exp=hdm05/all \
+                latent_dim=${DIM} beta=${BETA} iteration=${ITER} body_model=${MOD} > /dev/null 2>&1
         done
     done
 
