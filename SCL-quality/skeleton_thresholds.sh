ed4de8a360c2c04e15e824c9d019976377395129
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-10-18 23:08:45 +0200

    added script for measuring dtw threshold

--- SCL-quality/skeleton_thresholds.sh
+++ SCL-quality/skeleton_thresholds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=48:0:0
+#PBS -l walltime=24:0:0
 #PBS -l select=1:ncpus=16:mem=64gb
 #PBS -o /dev/null
 #PBS -e /dev/null
@@ -16,5 +16,20 @@ conda activate "/storage/brno12-cerit/home/drking/.conda/envs/${ENV_NAME}" || {
     exit 2
 }
 
-python3 dtw_matcher_new.py --input /storage/brno12-cerit/home/drking/data/hdm05/parts_norm/motion_hands_l_norm.npz \
-    --subset-size 2000 --subset-repeats 3 --n-jobs 8 --output /storage/brno12-cerit/home/drking/data/hdm05/parts_norm/percentile_hands_l_norm.json
+for FILE in "parts_norm/motion_hands_l_norm" "parts_norm/motion_hands_r_norm" "parts_norm/motion_legs_l_norm" "parts_norm/motion_legs_r_norm" "parts_norm/motion_torso_norm" "class130-actions-segment80_shift16-coords_normPOS-fps12"; do
+
+    python3 dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.npz \
+        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.json
+done
+
+for FILE in "cv_parts/motion_hands_l_norm" "cv_parts/motion_hands_r_norm" "cv_parts/motion_legs_l_norm" "cv_parts/motion_legs_r_norm" "cv_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
+
+    python3 dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cv-train \
+        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
+done
+
+for FILE in "cs_parts/motion_hands_l_norm" "cs_parts/motion_hands_r_norm" "cs_parts/motion_legs_l_norm" "cs_parts/motion_legs_r_norm" "cs_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
+
+    python3 dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cs-train \
+        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
+done
