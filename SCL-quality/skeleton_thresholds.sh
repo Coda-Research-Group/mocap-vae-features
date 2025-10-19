6e0a3d54fe313720764bddcdb2e3f57737dae896
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-10-19 13:23:42 +0200

    pku full set json colission solved

--- SCL-quality/skeleton_thresholds.sh
+++ SCL-quality/skeleton_thresholds.sh
@@ -16,20 +16,26 @@ conda activate "/storage/brno12-cerit/home/drking/.conda/envs/${ENV_NAME}" || {
     exit 2
 }
 
-for FILE in "parts_norm/motion_hands_l_norm" "parts_norm/motion_hands_r_norm" "parts_norm/motion_legs_l_norm" "parts_norm/motion_legs_r_norm" "parts_norm/motion_torso_norm" "class130-actions-segment80_shift16-coords_normPOS-fps12"; do
+# for FILE in "parts_norm/motion_hands_l_norm" "parts_norm/motion_hands_r_norm" "parts_norm/motion_legs_l_norm" "parts_norm/motion_legs_r_norm" "parts_norm/motion_torso_norm" "class130-actions-segment80_shift16-coords_normPOS-fps12"; do
 
-    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.npz \
-        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.json
-done
+#     python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.npz \
+#         --subset-size 3000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.json
+# done
 
-for FILE in "cv_parts/motion_hands_l_norm" "cv_parts/motion_hands_r_norm" "cv_parts/motion_legs_l_norm" "cv_parts/motion_legs_r_norm" "cv_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
+# for FILE in "cv_parts/motion_hands_l_norm" "cv_parts/motion_hands_r_norm" "cv_parts/motion_legs_l_norm" "cv_parts/motion_legs_r_norm" "cv_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
 
-    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cv-train \
-        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
-done
+#     python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cv-train \
+#         --subset-size 3000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
+# done
 
-for FILE in "cs_parts/motion_hands_l_norm" "cs_parts/motion_hands_r_norm" "cs_parts/motion_legs_l_norm" "cs_parts/motion_legs_r_norm" "cs_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
+# for FILE in "cs_parts/motion_hands_l_norm" "cs_parts/motion_hands_r_norm" "cs_parts/motion_legs_l_norm" "cs_parts/motion_legs_r_norm" "cs_parts/motion_torso_norm" "actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10"; do
 
-    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cs-train \
-        --subset-size 10000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
+#     python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.data-cs-train \
+#         --subset-size 3000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/${FILE}.json
+# done
+
+for EXP in "cs" "cv"; do
+    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/dtw_thresholds.py --input /storage/brno12-cerit/home/drking/data/pku-mmd/actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10.data-${EXP}-train \
+        --subset-size 3000 --subset-repeats 3 --n-jobs 16 --output /storage/brno12-cerit/home/drking/data/pku-mmd/actions_singlesubject-segment24_shift4.8_initialshift0-coords_normPOS-fps10-${EXP}.json
+
 done
