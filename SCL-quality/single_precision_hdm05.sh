668aac0fc2b0ddf7ea504eb4b5e07323321c424f
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-10-26 23:49:01 +0100

    hell yeah more

--- SCL-quality/single_precision_hdm05.sh
+++ SCL-quality/single_precision_hdm05.sh
@@ -11,9 +11,9 @@ SINGLE_JOB='/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL
 ITER=${ITERATION}
 DIM=${DIMENSION}
 BETA=${BETA}
-FILE=${FILE}
+DATA=${DATA}
 
-case "$FILE" in
+case "$DATA" in
   "parts_norm/motion_hands_l_norm") VAR="hdm05-handL" ;;
   "parts_norm/motion_hands_r_norm") VAR="hdm05-handR" ;;
   "parts_norm/motion_legs_l_norm")  VAR="hdm05-legL" ;;
@@ -21,7 +21,7 @@ case "$FILE" in
   "parts_norm/motion_torso_norm")   VAR="hdm05-torso" ;;
   "class130-actions-segment80_shift16-coords_normPOS-fps12") VAR="hdm05" ;;
   *)
-    echo "Unknown FILE: $FILE"
+    echo "Unknown DATA: $DATA"
     exit 1
     ;;
 esac
@@ -41,8 +41,8 @@ python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-qua
 wait 
 
 python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/SCL-quality/precision-recall.py \
-    /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.npz \
+    /storage/brno12-cerit/home/drking/data/hdm05/${DATA}.npz \
     /storage/brno12-cerit/home/drking/experiments/SCL/hdm05/all/model=${VAR}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data.gz \
-    /storage/brno12-cerit/home/drking/data/hdm05/${FILE}.json \
+    /storage/brno12-cerit/home/drking/data/hdm05/${DATA}.json \
     /storage/brno12-cerit/home/drking/experiments/SCL/hdm05/all/model=${VAR}_lat-dim=${DIM}_beta=${BETA}/${ITER}/scl.json \
     --dataset hdm05 --n-subsets 5 --subset-size 5000
