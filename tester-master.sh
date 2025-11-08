18d7aa1ca30c5bdc528cb99d5324a221b086166f
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-11-08 22:03:24 +0100

    hehe

--- tester-master.sh
+++ tester-master.sh
@@ -9,7 +9,8 @@ JDK_PATH='/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java'
 
 WORKER_SCRIPT_PATH="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single.sh"
 WORKER_SCRIPT_PATH_NORM="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-norm.sh"
-WORKER_SCRIPT_PATH_FULL="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-full.sh"
+WORKER_SCRIPT_PATH_FULL="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-hdm05.sh"
+WORKER_SCRIPT_PATH_PART="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-hdm05-parts.sh"
 WORKER_SCRIPT_PATH_SCL="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-scl.sh"
 WORKER_SCRIPT_PATH_MO="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-MO.sh"
 WORKER_SCRIPT_PATH_MO_PKU="/storage/brno12-cerit/home/drking/experiments/mocap-vae-features/tester-single-MO-pku.sh"
@@ -37,35 +38,70 @@ WORKER_SCRIPT_PATH_BASE="/storage/brno12-cerit/home/drking/experiments/mocap-vae
 #     done
 # done
 
-for EXP in "all"; do
-    for DIM in "32" "64"; do
-        for BETA in "0.1" "1" "10"; do
-            for ITER in 3; do
+# for EXP in "all"; do
+#     for DIM in "32" "64"; do
+#         for BETA in "0.1" "1" "10"; do
+#             for ITER in 3; do
 
-                JOB_NAME="evaluation_${EXP}_${ITER}_all"
+#                 JOB_NAME="evaluation_${EXP}_${ITER}_all"
+#                 qsub \
+#                     -N "${JOB_NAME}" \
+#                     -v "PASSED_ITER=${ITER},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
+#                     "${WORKER_SCRIPT_PATH}"
+
+#                 # JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_cv"
+#                 # echo "Submitting job for EXP=cv, DIM=${DIM}, BETA=${BETA}"
+#                 # qsub \
+#                 #     -N "${JOB_NAME}" \
+#                 #     -v "PASSED_EXP=cv,PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
+#                 #     "${WORKER_SCRIPT_PATH_MO_PKU}"
+
+#                 # JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_cs"
+#                 # echo "Submitting job for EXP=cs, DIM=${DIM}, BETA=${BETA}"
+#                 # qsub \
+#                 #     -N "${JOB_NAME}" \
+#                 #     -v "PASSED_EXP=cs,PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
+#                 #     "${WORKER_SCRIPT_PATH_MO_PKU}"
+
+#             done 
+#         done
+#     done
+# done
+
+for ITER in 1 2 3 4 5; do 
+    for BETA in "0.1" "1"; do 
+        for DIM in 32 64; do 
+
+            JOB_NAME="evaluation_${DIM}_${BETA}_${ITER}_all"
+
+            qsub \
+                -N "${JOB_NAME}" \
+                -v "PASSED_ITER=${ITER},PASSED_DIM=${DIM},PASSED_BETA=${BETA},PASSED_PART=hdm05" \
+                "${WORKER_SCRIPT_PATH_FULL}"
+
+        done
+    done   
+done
+
+for ITER in 3; do 
+    for BETA in "0.1" "1"; do 
+        for DIM in 8 16; do
+            for PART in "hdm05-handR" "hdm05-handL" "hdm05-legR" "hdm05-legL" "hdm05-torso"; do
+
+                JOB_NAME="evaluation_${DIM}_${BETA}_${ITER}_${part}"
+
                 qsub \
                     -N "${JOB_NAME}" \
-                    -v "PASSED_ITER=${ITER},PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                    "${WORKER_SCRIPT_PATH}"
+                    -v "PASSED_ITER=${ITER},PASSED_DIM=${DIM},PASSED_BETA=${BETA},PASSED_PART=${PART}" \
+                    "${WORKER_SCRIPT_PATH_PART}"
 
-                # JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_cv"
-                # echo "Submitting job for EXP=cv, DIM=${DIM}, BETA=${BETA}"
-                # qsub \
-                #     -N "${JOB_NAME}" \
-                #     -v "PASSED_EXP=cv,PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                #     "${WORKER_SCRIPT_PATH_MO_PKU}"
-
-                # JOB_NAME="evaluation_${EXP}_${DIM}_${BETA}_cs"
-                # echo "Submitting job for EXP=cs, DIM=${DIM}, BETA=${BETA}"
-                # qsub \
-                #     -N "${JOB_NAME}" \
-                #     -v "PASSED_EXP=cs,PASSED_DIM=${DIM},PASSED_BETA=${BETA}" \
-                #     "${WORKER_SCRIPT_PATH_MO_PKU}"
-
-            done 
+            done
         done
-    done
+    done   
 done
+
+
+
 
 # for EXP in "hdm05/all" "pku-mmd/cv" "pku-mmd/cs"; do
     # for DIM in "256" "128" "64" "32" "16" "8" ; do
---
+++ tester-single-hdm05-parts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#PBS -l walltime=12:0:0
+#PBS -l select=1:ncpus=4:mem=8gb
+#PBS -o /dev/null
+#PBS -e /dev/null
+
+
+JDK_PATH='/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java'
+
+DIM=${PASSED_DIM}
+BETA=${PASSED_BETA}
+EXP=${PASSED_EXP}
+PART=${PASSED_PART}
+
+
+for K in 10 20 35 50 60 80 100 150 200 250 300 350 400 500 750 1000 1250 1500 1750 2000 2500; do
+
+    rm "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/${PART}_lat_dim=${DIM}_beta=${BETA}/${K}/results.txt"
+
+    COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
+-fp /storage/brno12-cerit/home/drking/experiments/MWs/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/KMeansPivotChooser--kmeans.k_${K} \
+-dd /storage/brno12-cerit/home/drking/data/hdm05/category_description.txt \
+-k 4 \
+"
+    mkdir -p "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/${PART}_lat_dim=${DIM}_beta=${BETA}/${K}/"
+    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/${PART}_lat_dim=${DIM}_beta=${BETA}/results.txt"
+
+    COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
+-fp /storage/brno12-cerit/home/drking/experiments/MWs/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/KMeansPivotChooser--kmeans.k_${K} \
+-dd /storage/brno12-cerit/home/drking/data/hdm05/category_description.txt \
+"
+    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/all/${PART}_lat_dim=${DIM}_beta=${BETA}/results.txt"
+
+done
