663ea9b444a67f19bcc2def93a7b9d0a20113b9a
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-11-09 16:22:04 +0100

    results

--- tester-single-MO.sh
+++ tester-single-MO.sh
@@ -69,6 +69,8 @@ for K in 10 20 35 50 60 80 100 150 200 250 300 350 400 500 750 1000 1250 1500 17
 -dd /storage/brno12-cerit/home/drking/data/hdm05/category_description.txt \
 "
     echo "${COMMAND}"
+    mkdir -p "/storage/brno12-cerit/home/drking/experiments/results/hdm05/Multi-Overlay/"
+
     eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/results/hdm05/Multi-Overlay/full/dim=${DIM}_beta=${BETA}_${K}.txt"
 
 # recall
