30b2f84649f7d8736951d387c6f063411bc3db36
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-11-24 23:56:41 +0100

    quantization soft + non norm

--- quantization/master.sh
+++ quantization/master.sh
@@ -32,33 +32,33 @@ PART="hdm05"
 #         done
 #     done
 # done
-for PART in "hdm05-handR" "hdm05-handL" "hdm05-legR" "hdm05-legL" "hdm05-torso"; do
-    for ITER in 3; do
-        for BETA in "0.1" "1"; do
-            for DIM in 8 16 32 64; do
+# for PART in "hdm05-handR" "hdm05-handL" "hdm05-legR" "hdm05-legL" "hdm05-torso"; do
+#     for ITER in 3; do
+#         for BETA in "0.1" "1"; do
+#             for DIM in 8 16 32 64; do
 
-                gunzip -kf "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data.gz"
+#                 gunzip -kf "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data.gz"
 
-                # aby se smazal v pripade 2. pokusu ten predchozi.
-                rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
+#                 # aby se smazal v pripade 2. pokusu ten predchozi.
+#                 rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
 
-                perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
+#                 perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/hdm05/all/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
 
 
-                for K in 10 25 50 100 200 400 800 1600 3200;do 
+#                 for K in 10 25 50 100 200 400 800 1600 3200;do 
 
-                    JOB_NAME="q_${PART}_${DIM}_${BETA}_${ITER}_${K}"
+#                     JOB_NAME="q_${PART}_${DIM}_${BETA}_${ITER}_${K}"
 
-                    qsub \
-                        -N "${JOB_NAME}" \
-                        -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},PART=${PART}" \
-                        "${WHOLE_SCRIPT_PATH_HDM05}"
+#                     qsub \
+#                         -N "${JOB_NAME}" \
+#                         -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},PART=${PART}" \
+#                         "${WHOLE_SCRIPT_PATH_HDM05}"
 
-                done
-            done
-        done
-    done
-done
+#                 done
+#             done
+#         done
+#     done
+# done
 
 
 # PART="pku-mmd"
@@ -90,35 +90,35 @@ done
 #     done
 # done
 
-# for PART in "pku-mmd-handR" "pku-mmd-handL" "pku-mmd-legR" "pku-mmd-legL" "pku-mmd-torso"; do
-#     for SETUP in "cs" "cv"; do 
-#         for ITER in 3; do
-#             for BETA in "0.1" "1"; do
-#                 for DIM in 8 16 32 64; do
+for PART in "pku-mmd-handR" "pku-mmd-handL" "pku-mmd-legR" "pku-mmd-legL" "pku-mmd-torso"; do
+    for SETUP in "cs" "cv"; do 
+        for ITER in 3; do
+            for BETA in "0.1" "1"; do
+                for DIM in 8 16 32 64; do
 
-#                     python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/clustering/subset-maker.py "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data-train" --output "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data"
+                    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/clustering/subset-maker.py "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data-train" --output "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data"
 
-#                     # aby se smazal v pripade 2. pokusu ten predchozi.
-#                     rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
+                    # aby se smazal v pripade 2. pokusu ten predchozi.
+                    rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
 
-#                     perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
+                    perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
 
 
-#                     for K in 25 50 100 200 400 800 1600 3200 6400; do 
+                    for K in 25 50 100 200 400 800 1600 3200 6400; do 
 
-#                         JOB_NAME="q_${PART}_${SETUP}_${DIM}_${BETA}_${ITER}_${K}"
+                        JOB_NAME="q_${PART}_${SETUP}_${DIM}_${BETA}_${ITER}_${K}"
 
-#                         qsub \
-#                             -N "${JOB_NAME}" \
-#                             -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP},PART=${PART}" \
-#                             "${WHOLE_SCRIPT_PATH_PKU_MMD}"
+                        qsub \
+                            -N "${JOB_NAME}" \
+                            -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP},PART=${PART}" \
+                            "${WHOLE_SCRIPT_PATH_PKU_MMD}"
 
-#                     done
-#                 done
-#             done
-#         done
-#     done
-# done
+                    done
+                done
+            done
+        done
+    done
+done
 
 # PART="pku-mmd"
 # for SETUP in "cv"; do 
