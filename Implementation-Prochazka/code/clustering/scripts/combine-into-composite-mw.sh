2a38109d80932abe8fef3d28107df3af11ea0ae8
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-11-27 03:24:43 +0100

    scl remove mistakingly saved dtw

--- Implementation-Prochazka/code/clustering/scripts/combine-into-composite-mw.sh
+++ Implementation-Prochazka/code/clustering/scripts/combine-into-composite-mw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#PBS -l walltime=4:0:0
+#PBS -l select=1:ncpus=4:mem=16gb
 
 ## Defaults
 
@@ -28,16 +30,16 @@ ${JDK_PATH} \
 
 # HDM05-130
 for BETA in "0.1" "1"; do 
-    for DIM in 256; do 
-        for K in 100 200 400 800 1600; do
+    for DIM in 8 16 32 64; do 
+        for K in 10 25 50 100 200 400 800 1600 3200; do
         
             # BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/MWs/hdm05/all/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}"
             # combineBodyPartsIntoCompositeMW
 
-            # BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/MWs/hdm05/all/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}_non-norm"
-            # combineBodyPartsIntoCompositeMW
+            BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs-non-norm/hdm05/all/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}"
+            combineBodyPartsIntoCompositeMW
 
-            BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs/hdm05/all/grouped/model=hdm05_lat-dim=${DIM}_beta=${BETA}_k=${K}"
+            BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs/hdm05/all/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}"
             combineBodyPartsIntoCompositeMW
 
         done
@@ -46,11 +48,16 @@ done
 
 for SETUP in "cv" "cs"; do 
     for BETA in "0.1" "1"; do
-        for DIM in 256; do
-            for K in 100 200 400 800 1600 3200 6400 ; do 
+        for DIM in 8 16 32 64; do
+            for K in 25 50 100 200 400 800 1600 3200 6400 ; do 
 
-                    BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs/pku-mmd/${SETUP}/grouped/model=pku-mmd_lat-dim=${DIM}_beta=${BETA}_k=${K}"
+                    BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs-non-norm/pku-mmd/${SETUP}/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}"
                     combineBodyPartsIntoCompositeMW
+
+                    BODY_PART_MWS_FOLDER="/storage/brno12-cerit/home/drking/experiments/elki-MWs/pku-mmd/${SETUP}/grouped/group_lat-dim=${DIM}_beta=${BETA}_k=${K}"
+                    combineBodyPartsIntoCompositeMW
+
+
             done
         done
     done
