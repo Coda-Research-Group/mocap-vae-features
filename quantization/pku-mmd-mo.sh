b619b429a7703900a73bd34e0a92c4f32bffb729
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-12-03 13:15:30 +0100

    fixed multi overlay classification

--- quantization/pku-mmd-mo.sh
+++ quantization/pku-mmd-mo.sh
@@ -14,7 +14,7 @@ JDK_PATH="/storage/brno12-cerit/home/drking/jdk-21.0.7/bin/java"
 #######################################
 function single() {
 
-    rm -f "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results.txt"
+    # rm -f "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results.txt"
 
     COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
     -fp /storage/brno12-cerit/home/drking/experiments/elki-MWs/pku-mmd/${SETUP}/grouped/model=pku-mmd_lat-dim=${DIM}_beta=${BETA}_k=${K}.composite \
@@ -23,16 +23,16 @@ function single() {
     -k 18 \
     -${SETUP} \
     "
-    mkdir -p "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/"
-    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results.txt"
+    # mkdir -p "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/"
+    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results-classification.txt"
 
-    COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
-    -fp /storage/brno12-cerit/home/drking/experiments/elki-MWs/pku-mmd/${SETUP}/grouped/model=pku-mmd_lat-dim=${DIM}_beta=${BETA}_k=${K}.composite \
-    -dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
-    --nmatches 2 \
-    -${SETUP} \
-    "
-    eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results.txt"
+    # COMMAND="${JDK_PATH} -jar /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/evaluator.jar \
+    # -fp /storage/brno12-cerit/home/drking/experiments/elki-MWs/pku-mmd/${SETUP}/grouped/model=pku-mmd_lat-dim=${DIM}_beta=${BETA}_k=${K}.composite \
+    # -dd /storage/brno12-cerit/home/drking/data/pku-mmd/category_description.txt \
+    # --nmatches 2 \
+    # -${SETUP} \
+    # "
+    # eval "${COMMAND}" >> "/storage/brno12-cerit/home/drking/experiments/elki-results/pku-mmd/multi-overlay/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${K}/results-classification.txt"
 }
 
 for PART in "pku-mmd"; do
