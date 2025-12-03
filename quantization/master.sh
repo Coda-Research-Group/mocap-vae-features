c2be8bbd1a4d04f2b6967a0d710b56a624894976
Tomáš Drkoš <tom.drkos@seznam.cz>
2025-12-03 13:16:41 +0100

    fixed multi overlay classification

--- quantization/master.sh
+++ quantization/master.sh
@@ -89,36 +89,36 @@ PART="hdm05"
 #         done
 #     done
 # done
-7
-for PART in "pku-mmd-handR" "pku-mmd-handL" "pku-mmd-legR" "pku-mmd-legL" "pku-mmd-torso"; do
-    for SETUP in "cs" "cv"; do 
-        for ITER in 3; do
-            for BETA in "0.1" "1"; do
-                for DIM in 8 16 32 64; do
 
-                    python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/clustering/subset-maker.py "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data-train" --output "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data"
+# for PART in "pku-mmd-handR" "pku-mmd-handL" "pku-mmd-legR" "pku-mmd-legL" "pku-mmd-torso"; do
+#     for SETUP in "cs" "cv"; do 
+#         for ITER in 3; do
+#             for BETA in "0.1" "1"; do
+#                 for DIM in 8 16 32 64; do
 
-                    # aby se smazal v pripade 2. pokusu ten predchozi.
-                    rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
+#                     python3 /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/clustering/subset-maker.py "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented.data-train" --output "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data"
 
-                    perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
+#                     # aby se smazal v pripade 2. pokusu ten predchozi.
+#                     rm -f /storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data
+
+#                     perl /storage/brno12-cerit/home/drking/experiments/mocap-vae-features/Implementation-Prochazka/code/clustering/scripts/convert-from-messif.pl "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/predictions_segmented_subset.data" >> "/storage/brno12-cerit/home/drking/experiments/SCL-non-norm/pku-mmd/${SETUP}/model=${PART}_lat-dim=${DIM}_beta=${BETA}/${ITER}/elki-predictions_segmented.data"
 
 
-                    for K in 25 50 100 200 400 800 1600 3200 6400; do 
+#                     for K in 25 50 100 200 400 800 1600 3200 6400; do 
 
-                        JOB_NAME="q_${PART}_${SETUP}_${DIM}_${BETA}_${ITER}_${K}"
+#                         JOB_NAME="q_${PART}_${SETUP}_${DIM}_${BETA}_${ITER}_${K}"
 
-                        qsub \
-                            -N "${JOB_NAME}" \
-                            -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP},PART=${PART}" \
-                            "${WHOLE_SCRIPT_PATH_PKU_MMD}"
+#                         qsub \
+#                             -N "${JOB_NAME}" \
+#                             -v "ITER=${ITER},BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP},PART=${PART}" \
+#                             "${WHOLE_SCRIPT_PATH_PKU_MMD}"
 
-                    done
-                done
-            done
-        done
-    done
-done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
 # PART="pku-mmd"
 # for SETUP in "cv"; do 
@@ -184,18 +184,18 @@ done
 # done
 
 
-# for BETA in "0.1" "1"; do
-#     for DIM in 256; do
-#         for SETUP in "cs" "cv"; do
-#             for K in 100 200 400 800 1600; do 
-#                 JOB_NAME="mo-test_${PART}_${SETUP}_${DIM}_${BETA}_${K}"
+for BETA in "0.1" "1"; do
+    for DIM in 256; do
+        for SETUP in "cs" "cv"; do
+            for K in 100 200 400 800 1600; do 
+                JOB_NAME="mo-test_${PART}_${SETUP}_${DIM}_${BETA}_${K}"
 
-#                 qsub \
-#                     -N "${JOB_NAME}" \
-#                     -v "BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP}" \
-#                     "${WHOLE_SCRIPT_PATH_PKU_MMD_MO}"
+                qsub \
+                    -N "${JOB_NAME}" \
+                    -v "BETA=${BETA},DIM=${DIM},K=${K},SETUP=${SETUP}" \
+                    "${WHOLE_SCRIPT_PATH_PKU_MMD_MO}"
 
-#             done 
-#         done
-#     done
-# done
+            done 
+        done
+    done
+done
